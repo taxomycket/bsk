2acefb1ad75cf2f4b4fd939bdbcf18f3440a798e
taxomycket <mmiettin21@student.oulu.fi>
2024-10-28 19:46:42 -0700

    User story 9 - test 13

--- bowling.py
+++ bowling.py
@@ -26,14 +26,24 @@ class BowlingGame:
                 else:
                     frame.set_bonus(self._frames[i+1].get_first_throw())
             if frame.is_strike():
-                if self._frames[i+1].is_strike():
-                    frame.set_bonus(self._frames[i+1].get_first_throw()+self._frames[i+1].get_second_throw()+self._frames[i+2].get_first_throw())
+                if i == len(self._frames)-1:
+                    frame.set_bonus(self._bonus_throw + self._second_bonus_throw)
                 else:
-                    frame.set_bonus(self._frames[i+1].get_first_throw()+self._frames[i+1].get_second_throw())
+                    if i == len(self._frames)-2:
+                        if self._frame[i+1].is_strike():
+                            frame.set_bonus(
+                                self._frames[i + 1].get_first_throw() + self._frames[i + 1].get_second_throw() +
+                                self._bonus_throw()
+                            )
+
+                    if self._frames[i+1].is_strike():
+                        frame.set_bonus(self._frames[i+1].get_first_throw()+self._frames[i+1].get_second_throw()+self._frames[i+2].get_first_throw())
+                    else:
+                        frame.set_bonus(self._frames[i+1].get_first_throw()+self._frames[i+1].get_second_throw())
             score = score + frame.score()
         return score
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
         self._bonus_throw = bonus_throw
     def set_second_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        self._second_bonus_throw = bonus_throw
