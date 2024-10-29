d53cff5ba6c78d8dc7b92a58640f5a784fa21754
taxomycket <mmiettin21@student.oulu.fi>
2024-10-28 19:22:20 -0700

    User story 7 - test 10

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -130,3 +130,18 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(f)
         self.assertEqual(112, game.calculate_score())
 
+    def test_calculate_spare_followed_by_spare(self):
+        game = BowlingGame()
+        game.add_frame(Frame(8, 2))
+        game.add_frame(Frame(5, 5))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        f = Frame(2, 6)
+        game.add_frame(f)
+        self.assertEqual(98, game.calculate_score())
+
